f3a8ca78b5f74e22e65863044a1e426fe4d8f970
Камила <gmilashka555@gmail.com>
2023-01-31 17:11:28 +0200

    sims

---
+++ Less 3.py
@@ -0,0 +1,16 @@
+import random
+
+class Human:
+    def __init__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
+
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Mersedes")
+car.add_
