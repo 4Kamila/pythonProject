1a1fb59049f8821f4cc08babee8dd8a973b34c80
Камила <gmilashka555@gmail.com>
2023-02-13 07:28:35 +0200

    student         if self.pet_health >= 10:             pass         else:             self.vet()             return

--- HW 4.py
+++ HW 4.py
@@ -1,20 +1,39 @@
-class Adult:
-    height = 180
-    age = 32
-    contry = "Peru"
-    food = "pizza"
-class Teen(Adult):
-    height = 174
-    age = 15
-    food = "cheese"
-class Child(Teen):
-    food = "milk"
-    def __init__(self):
-        print(self.age)
-        print(self.height)
-        print(self.food)
-        print(self.contry)
-Ann = Adult()
-Micki = Teen()
-Tony = Child()
+# class Adult:
+#     height = 180
+#     age = 32
+#     contry = "Peru"
+#     food = "pizza"
+# class Teen(Adult):
+#     height = 174
+#     age = 15
+#     food = "cheese"
+# class Child(Teen):
+#     food = "milk"
+#     def __init__(self):
+#         print(self.age)
+#         print(self.height)
+#         print(self.food)
+#         print(self.contry)
+# Ann = Adult()
+# Micki = Teen()
+# Tony = Child()
+
+import random
+class Math:
+
+    def __number(self):
+        a = int(input("a = "))
+        b = int(input("b = "))
+        dice = random.randint(1, 4)
+        if dice == 1:
+            print(a + b)
+        elif dice == 2:
+            print(a - b)
+        elif dice == 3:
+            print(a * b)
+        elif dice == 4:
+            print(a / b)
+
+some_obj=Math()
+some_obj._Math__number()
 
