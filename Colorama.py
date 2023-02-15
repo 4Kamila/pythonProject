5406cdb324fd2fcdbbc0deec9d41bf4f9c1c3be7
Камила <gmilashka555@gmail.com>
2023-02-15 08:59:00 +0200

    student         if self.pet_health >= 10:             pass         else:             self.vet()             return

---
+++ Colorama.py
@@ -0,0 +1,52 @@
+import colorama
+
+# Ім'я бібліотеки
+
+# print(colorama.__name__)
+
+# Ім'я модуля
+
+# print(__name__)
+
+# Тип об'єкта
+
+# print(type(colorama))
+
+# Атрибути та методи
+
+# for method in dir():
+#     print(method)
+
+# Перевірка наявності атрибута чи метода в класі
+
+# data = "string"
+# print(hasattr(data, "index"))
+# print(hasattr(data, "reverse"))
+
+# Дізнатися чи можна викликати об'єкт
+
+# data = "string"
+# def first_function():
+#     pass
+# print(callable(first_function))
+# print(callable(data))
+
+# Дізнатися чи успадковується клас
+
+# class Class1:
+#     pass
+# class Class2(Class1):
+#     pass
+# print(issubclass(Class1, Class2))
+# print(issubclass(Class2, Class1))
+
+# Дізнатися чи належить об'єкт певному класу
+
+# class Class1:
+#     pass
+# class Class2(Class1):
+#     pass
+#
+# obj_from_class_2 = Class2()
+# print(isinstance(obj_from_class_2, Class1))
+# print(isinstance(obj_from_class_2, Class2))
